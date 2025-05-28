eb95e05b89673c5a3dfeb27448e7416de6216c04
jᥲrᥣιᥲᥲ <148370627+jarliaa@users.noreply.github.com>
2025-05-28 10:58:51 -0300

    8

--- script.js
+++ script.js
@@ -47,8 +47,8 @@ window.addEventListener('load', revealOnScroll);
 
 // Carrossel
 const images = [
-  './img/campo.png',
-  './img/malte.png',
+  './img/vacas.jpg',
+  './img/pr0oducao.jpg',
   './img/cidade.png'
 ];
 let currentIndex = 0;
